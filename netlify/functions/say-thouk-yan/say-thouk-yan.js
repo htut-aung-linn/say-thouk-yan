e115b5fe87f8f9a3e678dbc366d06474cf1938fd
Htut Aung Linn <123470908+htut-aung-linn@users.noreply.github.com>
2024-05-22 01:59:10 -0700

    Update say-thouk-yan.js

--- netlify/functions/say-thouk-yan/say-thouk-yan.js
+++ netlify/functions/say-thouk-yan/say-thouk-yan.js
@@ -1,12 +1,13 @@
 const { schedule } = require('@netlify/functions')
+import fetch from 'node-fetch';
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
+
 module.exports.handler = schedule('*/5 * * * *', async (event) => {
   const eventBody = JSON.parse(event.body)
   //  http://test-project-h.000.pe/Update.php
   // Making an HTTP GET request using fetch
   console.log('init node fetch');
-  const fetch = require('node-fetch');
   const url = 'https://test-project-h.000.pe/Update.php';
   try {
     console.log('calling link');
