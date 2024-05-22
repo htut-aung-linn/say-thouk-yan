1698452c4d73a5f58fa48736267de63c20141760
Htut Aung Ling <htutlinn686@gmail.com>
2024-05-22 00:21:00 -0700

    clall link function

--- netlify/functions/say-thouk-yan/say-thouk-yan.js
+++ netlify/functions/say-thouk-yan/say-thouk-yan.js
@@ -2,7 +2,7 @@ const { schedule } = require('@netlify/functions')
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
-module.exports.handler = schedule('15 * * * *', async (event) => {
+module.exports.handler = schedule('0 * * * *', async (event) => {
   const eventBody = JSON.parse(event.body)
   console.log(`Next function run at ${eventBody.next_run}.`)
 
